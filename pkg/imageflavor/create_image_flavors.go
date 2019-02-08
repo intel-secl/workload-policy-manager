aac3a9b830852fbcc1e9e0ae30aab9eb37bd2d86
Gowda, Harshitha M <harshitha.m.gowda@intel.com>
2019-02-08 15:22:50 -0800

    V1.0/features/add key url in image flavor

--- pkg/imageflavor/create_image_flavors.go
+++ pkg/imageflavor/create_image_flavors.go
@@ -13,17 +13,18 @@ import (
 	log "github.com/sirupsen/logrus"
 	flavor "intel/isecl/lib/flavor"
 	kms "intel/isecl/lib/kms-client"
+	config "intel/isecl/wpm/config"
 	"intel/isecl/wpm/consts"
 	"intel/isecl/wpm/pkg/kmsclient"
 	"io/ioutil"
 	"os"
 	"strings"
+	"net/url"
 )
 
 //CreateImageFlavor is used to create flavor of an encrypted image
 func CreateImageFlavor(label string, imagePath string, encryptFilePath string, keyID string, encryptionRequired bool, integrityEnforced bool, outputFile string) (string, error) {
 	var err error
-	var keyURL string
 	var keyValue []byte
 	var keyInfo kms.KeyInfo
 
@@ -61,21 +62,27 @@ func CreateImageFlavor(label string, imagePath string, encryptFilePath string, k
 
 		key, err := kc.Keys().Create(keyInfo)
 		if err != nil {
-			return "", errors.New("error in creating transfer key. " + err.Error())
+			return "", errors.New("error in creating transfer key: " + err.Error())
 		}
 		keyID = key.KeyID
+	}
+
+	// set key URL
+	keyURL, err := url.Parse(config.Configuration.Kms.APIURL + "keys/" + keyID + "/transfer")
+	if err != nil {
+		return "", errors.New("error building KMS key URL: " + err.Error())
 	}
 
 	//retrieve key using keyid
 	keyValue, err = kc.Key(keyID).Retrieve()
 	if err != nil {
-		return "", errors.New("error in retrieving transfer key. " + err.Error())
+		return "", errors.New("error in retrieving transfer key: " + err.Error())
 	}
 
 	// encrypt image using key
 	err = encrypt(imagePath, consts.EnvelopePrivatekeyLocation, encryptFilePath, keyValue)
 	if err != nil {
-		return "", errors.New("error in encrypting image. " + err.Error())
+		return "", errors.New("error in encrypting image: " + err.Error())
 	}
 	encryptedImage, err := ioutil.ReadFile(encryptFilePath)
 	if err != nil {
@@ -85,22 +92,18 @@ func CreateImageFlavor(label string, imagePath string, encryptFilePath string, k
 	digest := sha256.Sum256([]byte(encryptedImage))
 
 	// create image flavor
-	imageFlavor, err := flavor.GetImageFlavor(label, encryptionRequired, keyURL, base64.StdEncoding.EncodeToString(digest[:]))
+	imageFlavor, err := flavor.GetImageFlavor(label, encryptionRequired, keyURL.String(), base64.StdEncoding.EncodeToString(digest[:]))
 	if err != nil {
-		return "", errors.New("error in creating image flavor." + err.Error())
+		return "", errors.New("error in creating image flavor:" + err.Error())
 	}
 
 	jsonFlavor, err := json.Marshal(imageFlavor)
+
 	if len(strings.TrimSpace(outputFile)) <= 0 {
 		return string(jsonFlavor), nil
 	}
-	//create outputFile for image flavor
-	_, err = os.Create(outputFile)
-	if err != nil {
-		return "", errors.New("error creating output file. " + err.Error())
-	}
-
-	err = ioutil.WriteFile(outputFile, []byte(jsonFlavor), 0600)
+	
+	err = ioutil.WriteFile(outputFile, jsonFlavor, 0600)
 	if err != nil {
 		return "", errors.New("error writing image flavor to output file")
 	}
