b8a6a602b2917909ac9323e16b75bc42c0b0ca48
Arijit Ghosh <arijit.ghosh@intel.com>
2020-11-06 12:50:18 +0530

    v3.2/bug/ISECL-12293: Add logrotate config only for log files and not the entire directory

--- dist/linux/install.sh
+++ dist/linux/install.sh
@@ -145,16 +145,16 @@ export LOG_OLD=${LOG_OLD:-12}
 mkdir -p /etc/logrotate.d
 
 if [ ! -a /etc/logrotate.d/wpm ]; then
- echo "/var/log/workload-policy-manager/* {
+ echo "/var/log/workload-policy-manager/*.log {
     missingok
-        notifempty
-        rotate $LOG_OLD
-        maxsize $LOG_SIZE
+    notifempty
+    rotate $LOG_OLD
+    maxsize $LOG_SIZE
     nodateext
-        $LOG_ROTATION_PERIOD
-        $LOG_COMPRESS
-        $LOG_DELAYCOMPRESS
-        $LOG_COPYTRUNCATE
+    $LOG_ROTATION_PERIOD
+    $LOG_COMPRESS
+    $LOG_DELAYCOMPRESS
+    $LOG_COPYTRUNCATE
 }" > /etc/logrotate.d/wpm
 fi
 
